bf02205221a7f3821ec6f8acf65c28ac0e1ffc5e
Michael Lamparski <diagonaldevice@gmail.com>
2017-05-26 11:28:33 -0400

    add WIP band unfolding function
    far far simpler than the folding function
    
    unfortunately I got caught up in trying to fix the test suite,
    because it is so ungodly slow for reasons I don't understand.

--- src/Band/Fold.hs
+++ src/Band/Fold.hs
@@ -6,12 +6,12 @@
 
 module Band.Fold(
     foldBandComputation,
+    unfoldBandComputation,
     testSuite_49db2cfe_158e_40ac_9399_1d5a07ffae96,
     ) where
 
 import           "exphp-prelude" ExpHPrelude hiding (putStr, transpose)
 import           "base" Data.Monoid(All(..))
-import           "base" Data.Fixed(mod')
 import           "base" Data.List.NonEmpty(NonEmpty)
 import           "base" Data.Ratio(denominator)
 import           "linear" Linear.V3
@@ -32,8 +32,8 @@ type IMat = M33 Z
 -- TODO: Unit test
 
 -- | Given two structures where the lattice of one is an exact superlattice
---   of the other, compute a band structure for a the larger structure by
---   folding bands from the smaller structure into the larger structure's FBZ.
+--   of the other, compute a band structure for a the LARGER structure by
+--   folding bands from the SMALLER structure into the larger structure's FBZ.
 --
 -- ("fold" is used here in the physics sense; not the Haskell sense)
 foldBandComputation :: (Integral n, Monad m, Real x, Fractional x)
@@ -50,6 +50,25 @@ foldBandComputation cMat unitCompute qs = foldBandComputation_ cMat' unitCompute
     unitCompute' = unitCompute . fmap (fmap realToFrac) . fmap toList
     qs' = fmap realToFrac . toV3 <$> qs
 
+-- | Given two structures where the lattice of one is an exact superlattice
+--   of the other, compute a band structure for a the SMALLER structure by
+--   unfolding bands from the LARGER structure into the smaller structure's FBZ.
+--
+-- ("fold" is used here in the physics sense; not the Haskell sense)
+unfoldBandComputation :: (Integral n, Monad m, Real x, Fractional x)
+                    => [[n]]              -- ^ row-based 3x3 supercell matrix C satisfying S = C A, where A and S
+                                          --   are unit cells for the smaller and larger systems, respectively.
+                                          --   (any missing entries are filled with the identity matrix)
+                    -> ([[x]] -> m [[a]]) -- ^ callback to compute bands in the smaller system, taking fractional
+                                          --   reciprocal-space coords and producing a list of values at each.
+                    -> ([[x]] -> m [[a]]) -- ^ result is a similar function but for the larger system.
+                                          --   Items within each inner list are arbitrarily ordered.
+unfoldBandComputation cMat superCompute qs = foldBandComputation_ cMat' superCompute' qs'
+  where
+    cMat' = toM33 $ fmap (fmap fromIntegral) cMat
+    superCompute' = superCompute . fmap (fmap realToFrac) . fmap toList
+    qs' = fmap realToFrac . toV3 <$> qs
+
 toV3 :: (Num a)=> [a] -> (V3 a)
 toV3  [a]     = V3 a 0 0
 toV3  [a,b]   = V3 a b 0
@@ -62,7 +81,6 @@ toM33 [a,b,c] = V3 (toV3 a) (toV3 b)   (toV3 c)
 toM33 _ = error "invalid 3D matrix"
 
 -- | Internal version using types from "linear".
---   Exposed for testing...
 foldBandComputation_ :: (Monad m)
                      => IMat
                      -> ([QVec Q] -> m [[a]])
@@ -77,14 +95,24 @@ foldBandComputation_ cMat unitCompute = superCompute
                     & fmap snd . IntMap.toAscList -- order by superQs index
         let (Just perSuper) = onlyUniqueValue (fromIntegral . length <$> superEs)
         let (Just perUnit)  = onlyUniqueValue (fromIntegral . length <$> unitEs)
-        let !() = reallyAssert (perUnit * det33 cMat == perSuper) ()
+        let !() = reallyAssert (perUnit * volume cMat == perSuper) ()
         pure superEs
 
     allImages :: [(label, QVec Q)] -> [(label, QVec Q)]
-    allImages qs = [ (label, modBy' 1 <$> (gamma' + (q *! kMat))) | gamma' <- supercellGammas cMat
-                                                        , (label, q) <- qs
-                                                        ]
-    kMat = inv33 (fmap fromIntegral <$> transpose cMat)
+    allImages qs = [ (label, mod1 <$> (gamma' + (q *! kMat))) | gamma' <- supercellGammas cMat
+                                                              , (label, q) <- qs
+                                                              ]
+    kMat = inv33 . transpose . fmap (fmap fromIntegral) $ cMat
+
+-- | Internal version using types from "linear".
+unfoldBandComputation_ :: (Monad m)
+                       => IMat
+                       -> ([QVec Q] -> m [[a]])
+                       -> ([QVec Q] -> m [[a]])
+unfoldBandComputation_ cMat superCompute = unitCompute
+  where
+    unitCompute unitQs = superCompute $ fmap mod1 <$> (unitQs !*! kInv)
+    kInv = transpose . fmap (fmap fromIntegral) $ cMat
 
 supercellGammas :: IMat     -- supercell matrix C (row-based) such that S = C A
                 -> [QVec Q] -- fractional points in K_A which are images of Gamma in K_S
@@ -94,50 +122,33 @@ supercellGammas cMat =
                   , ic <- fromIntegral <$> (0 ..< nc)
                   ] !*! kMat
   where
-    kMat = inv33 (fmap fromIntegral <$> transpose cMat)
+    kMat = inv33 . transpose . fmap (fmap fromIntegral) $ cMat
     V3 na nb nc = rationalMatrixPeriods kMat
 
--- (you hear the author of the code muttering something unintelligible
---  about "infix operators" and "footguns"...)
-modBy' :: (Real a)=> a -> a -> a
-modBy' = flip Data.Fixed.mod'
+mod1 :: (RealFrac a)=> a -> a
+mod1 x = x - fromInteger (floor x)
+
+-- stop forgetting to take the absolute value!
+volume :: M33 Z -> Z
+volume = abs . det33
 
 infix 0 ..<
 (..<) :: (Enum b)=> b -> b -> [b]
 a ..< b = [a .. pred b]
-------------------------------------------------------------------
-------------------------------------------------------------------
 
-testSuite_49db2cfe_158e_40ac_9399_1d5a07ffae96 :: TestTree
-testSuite_49db2cfe_158e_40ac_9399_1d5a07ffae96 =
-    "Subst" ~:
-    [ "supercellGammas" ~:
-        [ "trivial case" ~: supercellGammas identity @?= [V3 0 0 0]
-        ]
-    , "foldBandComputation_" ~:
-        [ "quickCheck identity"  ~: qc propIdentity
-        , "quickCheck identity_" ~: qc propIdentity_
-        ]
-    ]
+------------------------------------------------------------------
+------------------------------------------------------------------
 
 -- make it easier to have quickcheck generate lists of equal length
-type R7 = (R,R,R,R,R,R,R)
+type R7 = (R,R,R,R,R)
+type W3  = (Word8,Word8,Word8)
+type W33 = (W3,W3,W3)
+uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
+uncurry3 f (a,b,c) = f a b c
+i33ToM33 :: W33 -> M33 Z
+i33ToM33 = fmap (fmap (fromIntegral . (`mod` 16))) . fmap (uncurry3 V3) . uncurry3 V3
 r7ToList :: R7 -> [R]
-r7ToList (a,b,c,d,e,f,g) = [a,b,c,d,e,f,g]
-
-propIdentity :: Fun (Q,Q,Q) (Identity R7) -> (NonEmpty (Q,Q,Q)) -> Property
-propIdentity f qs =
-    f' qs' === foldBandComputation [[1::Integer,0],[0,1]] f' qs'
-  where
-    f' = mapM (fmap r7ToList . apply f . (\[a,b,c] -> (a,b,c)))
-    qs' = fmap (`mod'` 1) . (\(a,b,c) -> [a, b, c]) <$> toList qs
-
-propIdentity_ :: Fun (Q,Q,Q) (Identity R7) -> (NonEmpty (Q,Q,Q)) -> Property
-propIdentity_ f qs =
-    f' qs' === foldBandComputation_ identity f' qs'
-  where
-    f' = mapM (fmap r7ToList . apply f . unV3)
-    qs' = fmap (`mod'` 1) . enV3 <$> toList qs
+r7ToList (a,b,c,d,e) = [a,b,c,d,e]
 
 type TestableV3 a = (a,a,a) -- V3 analogue with Arbitrary/Coarbitrary instances
 -- type TestableM33 a = TestableV3 (TestableV3 a)
@@ -146,6 +157,56 @@ enV3 :: TestableV3 a -> V3 a
 unV3 :: V3 a -> TestableV3 a
 enV3 (a,b,c) = V3 a b c
 unV3 (V3 a b c) = (a,b,c)
+
+-- FIXME these tests are ungodly slow. Profiling suggests that most of the time
+--         is spent in constructing Fun objects
+testSuite_49db2cfe_158e_40ac_9399_1d5a07ffae96 :: TestTree
+testSuite_49db2cfe_158e_40ac_9399_1d5a07ffae96 =
+    -- FIXME the identity matrix doesn't exactly make for a good regression test.
+    "Subst" ~:
+
+    [ "identity matrix" ~:
+        [ "supercellGammas" ~: supercellGammas identity @?= [V3 0 0 0]
+        , "foldBandComputation"    ~: qc propIdentityFold
+        , "foldBandComputation_"   ~: qc propIdentityFold_
+        , "unfoldBandComputation"  ~: qc propIdentityUnfold
+        , "unfoldBandComputation_" ~: qc propIdentityUnfold_
+        ]
+
+    -- FIXME too slow
+--    , "relationships" ~:
+--        [ "unfold-then-fold" ~: qc propUnfoldFold -- FIXME too slow.
+--        ] :: [TestTree]
+
+    ]
+  where
+    propIdentityFold    :: Fun (Q,Q,Q) (Identity R7) -> (NonEmpty (Q,Q,Q)) -> Property
+    propIdentityFold_   :: Fun (Q,Q,Q) (Identity R7) -> (NonEmpty (Q,Q,Q)) -> Property
+    propIdentityUnfold  :: Fun (Q,Q,Q) (Identity R7) -> (NonEmpty (Q,Q,Q)) -> Property
+    propIdentityUnfold_ :: Fun (Q,Q,Q) (Identity R7) -> (NonEmpty (Q,Q,Q)) -> Property
+
+    propIdentityFold    f qs = (getF  f) (getQs  qs) === foldBandComputation    eye  (getF  f) (getQs  qs)
+    propIdentityFold_   f qs = (getF_ f) (getQs_ qs) === foldBandComputation_   eye_ (getF_ f) (getQs_ qs)
+    propIdentityUnfold  f qs = (getF  f) (getQs  qs) === unfoldBandComputation  eye  (getF  f) (getQs  qs)
+    propIdentityUnfold_ f qs = (getF_ f) (getQs_ qs) === unfoldBandComputation_ eye_ (getF_ f) (getQs_ qs)
+
+    propUnfoldFold :: W33 -> Fun (Q,Q,Q) (Identity R7) -> (NonEmpty (Q,Q,Q)) -> Property
+    propUnfoldFold cMat f qs =
+        let (c', f', qs') = (getC_ cMat, getF_ f, take 20 $ getQs_ qs) in
+        acceptableCmat c' ==> -- traceShow (volume c', (show f), length qs) $
+            ((===) `on` fmap (fmap sort)) (fmap (cycleNTimes (fromIntegral $ volume c')) <$> f' qs')
+                                          (foldBandComputation_ c' (unfoldBandComputation_ c' f') qs')
+
+    cycleNTimes n xs = take (length xs * n) (cycle xs)
+
+    acceptableCmat c = (volume c /= 0) && (volume c < 1000)
+    eye = [[1::Integer, 0], [0, 1]]
+    eye_ = identity
+    getF  f  = mapM (fmap r7ToList . apply f . (\[a,b,c] -> (a,b,c)))
+    getQs qs = fmap mod1 . (\(a,b,c) -> [a, b, c]) <$> toList qs
+    getF_  f  = mapM (fmap r7ToList . apply f . unV3)
+    getQs_ qs = fmap mod1 . enV3 <$> toList qs
+    getC_ = i33ToM33
 
 ---------------------------------------------------
 
